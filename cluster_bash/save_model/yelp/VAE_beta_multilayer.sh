332eb413d214065b6f1a6f55d5aad718c9cf83c1
TinaBBB <tinashen.tianshu@gmail.com>
2021-08-20 11:58:42 -0400

    change directory for bash scripts

--- cluster_bash/save_model/yelp/VAE_beta_multilayer.sh
+++ cluster_bash/save_model/yelp/VAE_beta_multilayer.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 module load python/3.7
-source ~/ENV/bin/activate
+source ~/vae_uncertainty/bin/activate
 
-cd /home/hojin/projects/def-ssanner/hojin/vae-pe
+cd /home/tinashen/projects/def-ssanner/tinashen/bayesian-critiquing-recommender
+
 #cd ~/code/vae-pe
 python model_save.py --model_name VAEmultilayer --data_name yelp --data_dir fold0 --log_dir VAE_beta_multilayer --conf VAE_beta_multilayer.config
 python model_save.py --model_name VAEmultilayer --data_name yelp --data_dir fold1 --log_dir VAE_beta_multilayer --conf VAE_beta_multilayer.config
